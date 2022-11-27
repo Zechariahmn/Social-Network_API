5d90d81242f041ef72eece5c45618fd6fb60e41b
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-27 14:48:29 -0600

    added js code to import all api routes from the index.js file in the api folder

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// Imports all API routes from api folder index.js file
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+router.use((req, res) => res.send('Wrong route!'));
+module.exports = router;
