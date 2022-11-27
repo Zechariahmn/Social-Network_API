1ed30fd84da06099b5eb27dff268e6a4bdf9507d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-27 15:05:27 -0600

    added route to POST and DELETE a friend by id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -18,3 +18,10 @@ router.route('/:userId')
 .get(getSingleUser)
 .put(updateUser)
 .delete(deleteUser);
+
+//POST and DELETE a friend by ID
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
