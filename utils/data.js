bc4bacf3dedafd3366f69f404ab4dc666e8db4bd
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-04 16:32:59 -0500

    added code to the data, helpers and seeds js files

---
+++ utils/data.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+    "username": "lernantino",
+    "email": "lernantino@gmail.com",
+    "thoughts": [],
+    "friends": [],
+    "__v": 0
+},
+{
+      "username": "tester",
+      "email": "test@test.com",
+      "thoughts": [],
+      "friends": [],
+      "__v": 0
+    }
+  ];
+
+  const thoughts = [
+  {
+    "thoughtText": "Here's a cool thought...",
+    "username": "lernantino",
+    "userId": "5edff358a0fcb779aa7b118b"
+  }
+]
