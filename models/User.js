d0ef6a7a64842dc012f726ce3177528058ea49a8
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-03 23:30:55 -0500

    added email field to user js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,20 @@ const moment = require('moment');
 
 const userSchema = new Schema(
     {
-      // username field
+      
+    // username field
       username: {
         type: String,
         unique: true,
         required: true,
         trim: true,
+      },
+
+      //email field
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
       },
