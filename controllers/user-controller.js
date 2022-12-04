72f4cf24c39022d1d58b8944dacbc1a7eef3d004
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-04 00:24:35 -0500

    added the ability to get all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const { User } = require('../models')
+
+const userController = {
+    //get all users
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+         })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        });
+      },
+
+      
