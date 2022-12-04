6a69ec6038ef25aaace31ed0cf0f2eea58e5313d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-04 16:23:45 -0500

    added utils folder and files

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //mongodb connection to the application
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB', {
-  useFindAndModify: false,
+  //useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
