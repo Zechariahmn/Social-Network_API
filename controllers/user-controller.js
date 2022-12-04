5c40360a702c4be255ee180a1c89a1afeab1a3a3
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-04 00:29:41 -0500

    added the ability to update users by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -50,7 +50,23 @@ const userController = {
   //create User
   createUser({ body }, res) {
     User.create(body)
+    
     .then(dbUserData => res.json(dbUserData))
+    
     .catch(err => res.status(400).json(err));
 },
 
+ // the updateUser method updates a user by their id
+ updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      
+    .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      
+      .catch((err) => res.status(400).json(err));
+  },
