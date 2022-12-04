bf0bbd96f0ee05f448f7149866989d222117cac6
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-03 23:09:23 -0500

    created reaction-count that retrieves the total count of friends

--- models/thought.js
+++ models/thought.js
@@ -67,3 +67,12 @@ const ReactionSchema = new Schema(
   }
 )
 
+// get total count of friends on retrieval
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+  const Thought = model('Thought', ThoughtSchema);
+
+  module.exports = Thought;
+
