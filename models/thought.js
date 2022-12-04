0f94f9ee43ecc26c5e7d761120850189f248bed9
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-03 22:44:07 -0500

    added the reaction-id to thoughts js

---
+++ models/thought.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+
+      
