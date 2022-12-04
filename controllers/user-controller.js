bb71a687d65b45db0d8133d0ce0870e3fb650cc7
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-04 00:30:36 -0500

    added the ability to delete users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -70,3 +70,19 @@ const userController = {
       
       .catch((err) => res.status(400).json(err));
   },
+
+  //delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+    
+    .then(dbUserData => {
+    if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+    }
+    res.json(dbUserData);
+    })
+    
+    .catch(err => res.status(400).json(err))
+},
+
