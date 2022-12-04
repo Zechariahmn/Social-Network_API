3714e57b68caa70bf78d61952b72c8512a044284
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-03 23:19:08 -0500

    added comments to thoughts js for thoughts and reaction schema code

--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,7 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+// reaction schema only
 const ReactionSchema = new Schema(
     {
       // set custom id to avoid confusion with parent comment _id
@@ -9,6 +10,7 @@ const ReactionSchema = new Schema(
         default: () => new Types.ObjectId()
       },
 
+      //has a min. of 1 and a max. of 280
       reactionBody: {
         type: String,
         required: true,
@@ -17,17 +19,20 @@ const ReactionSchema = new Schema(
         maxlength: 280
       },
 
+      //Username field
       username: {
         type: String,
         required: true,
       },
 
+      //all reactions will have a time stamp in a specific format on query
       createdAt: {
         type: Date,
         default: Date.now,
         get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
       }
     },
+
     {
       toJSON: {
         getters: true
@@ -35,8 +40,11 @@ const ReactionSchema = new Schema(
     }
   );
 
+  //thought schema
   const ThoughtSchema = new Schema (
     {
+        
+        //thought-text field that has a min. of 1 and a max. of 280
         thoughtText: {
             type: String,
             required: true,
@@ -44,25 +52,30 @@ const ReactionSchema = new Schema(
             maxlength: 280
         },
 
+        // CreatedAt field will be time stamped with the current time
         createdAt: {
             type: Date,
             default: Date.now,
             get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         },
 
+        //username field
         username: {
             type: String,
             required: true,
             ref: 'User'
         },
 
+        //// array of nested documents created with the reactionSchema
         reactions: [ReactionSchema],
     },
+
     {
       toJSON: {
         virtuals: true,
         getters: true
       },
+
       id: false
   }
 )
